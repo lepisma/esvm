1b52848c18c005bb56b19319a9224ad1f06c2fe3
Abhinav Tushar <lepisma@fastmail.com>
2018-01-16 23:56:08 -0500

    remove malloc casts and fix types

--- esvm-core.c
+++ esvm-core.c
@@ -16,7 +16,7 @@ vref (emacs_env *env, emacs_value vec, int i)
   return env->vec_get(env, vec, i);
 }
 
-static float
+static double
 vref_f (emacs_env *env, emacs_value vec, int i)
 {
   return env->extract_float(env, vref(env, vec, i));
@@ -28,14 +28,14 @@ vref2 (emacs_env *env, emacs_value vec, int i, int j)
   return vref(env, vref(env, vec, i), j);
 }
 
-static float
+static double
 vref2_f (emacs_env *env, emacs_value vec, int i, int j)
 {
   return env->extract_float(env, vref2(env, vec, i, j));
 }
 
 static emacs_value
-make_vector (emacs_env *env, int len, float init)
+make_vector (emacs_env *env, int len, double init)
 {
   emacs_value Fmake_vector = env->intern(env, "make-vector");
   emacs_value args[] = { env->make_integer(env, len), env->make_float(env, init) };
@@ -45,10 +45,11 @@ make_vector (emacs_env *env, int len, float init)
 static emacs_value
 Fgo (emacs_env *env, ptrdiff_t n, emacs_value args[], void *data)
 {
-  struct svm_parameter params;
-  struct svm_problem prob;
+  struct svm_parameter parameters;
+  struct svm_problem problem;
   struct svm_model *model;
-  struct svm_node *x_space;
+  struct svm_node *train_nodes;
+  struct svm_node *test_nodes;
 
   emacs_value x_train = args[0];
   emacs_value y_train = args[1];
@@ -56,64 +57,64 @@ Fgo (emacs_env *env, ptrdiff_t n, emacs_value args[], void *data)
 
   int train_size = env->vec_size(env, x_train);
   int test_size = env->vec_size(env, x_test);
-  int features = env->vec_size(env, vref(env, x_train, 0));
+  int n_features = env->vec_size(env, vref(env, x_train, 0));
 
   emacs_value y_out = make_vector(env, test_size, 0.0);
 
-  prob.l = train_size;
-  prob.y = (double *) malloc(train_size * sizeof(double));
-  prob.x = (struct svm_node **) malloc(train_size * sizeof(struct svm_node *));
-  x_space = (struct svm_node *) malloc(train_size * (features + 1) * sizeof(struct svm_node));
+  problem.l = train_size;
+  problem.y = malloc(train_size * sizeof(double));
+  problem.x = malloc(train_size * sizeof(struct svm_node *));
+  train_nodes = malloc(train_size * (n_features + 1) * sizeof(struct svm_node));
 
-  for (int i = 0; i < prob.l; i++)
+  for (int i = 0; i < problem.l; i++)
     {
-      prob.y[i] = vref_f(env, y_train, i);
+      problem.y[i] = vref_f(env, y_train, i);
     }
 
   int j = 0;
-  for (int i = 0; i < prob.l; i++)
+  for (int i = 0; i < problem.l; i++)
     {
-      prob.x[i] = &x_space[j];
-      for (int k = 0; k < features; ++k, ++j) {
-        x_space[j].index = k + 1;
-        x_space[j].value = vref2_f(env, x_train, i, k);
+      problem.x[i] = &train_nodes[j];
+      for (int k = 0; k < n_features; k++, j++) {
+        train_nodes[j].index = k + 1;
+        train_nodes[j].value = vref2_f(env, x_train, i, k);
       }
-      x_space[j].index = -1;
-      x_space[j].value = 0;
+      train_nodes[j].index = -1;
+      train_nodes[j].value = 0;
       j++;
     }
 
-  params.svm_type = C_SVC;
-  params.kernel_type = RBF;
-  params.degree = 3;
-  params.gamma = 0.001;
-  params.coef0 = 0;
-  params.nu = 0.5;
-  params.cache_size = 100;
-  params.C = 1;
-  params.eps = 1e-3;
-  params.p = 0.1;
-  params.shrinking = 1;
-  params.probability = 0;
-  params.nr_weight = 0;
-  params.weight_label = NULL;
-  params.weight = NULL;
+  parameters.svm_type = C_SVC;
+  parameters.kernel_type = RBF;
+  parameters.degree = 3;
+  parameters.gamma = 0.001;
+  parameters.coef0 = 0;
+  parameters.nu = 0.5;
+  parameters.cache_size = 100;
+  parameters.C = 1;
+  parameters.eps = 1e-3;
+  parameters.p = 0.1;
+  parameters.shrinking = 1;
+  parameters.probability = 0;
+  parameters.nr_weight = 0;
+  parameters.weight_label = NULL;
+  parameters.weight = NULL;
 
-  model = svm_train(&prob, &params);
+  model = svm_train(&problem, &parameters);
 
   // Predict
   j = 0;
-  struct svm_node *x_space_test = (struct svm_node *) malloc((features + 1) * sizeof(struct svm_node));
+  test_nodes = malloc((n_features + 1) * sizeof(struct svm_node));
   for (int i = 0; i < test_size; i++)
     {
-      for (j = 0; j < features; ++j)
+      for (j = 0; j < n_features; j++)
         {
-          x_space_test[j].index = j + 1;
-          x_space_test[j].value = vref2_f(env, x_test, i, j);
+          test_nodes[j].index = j + 1;
+          test_nodes[j].value = vref2_f(env, x_test, i, j);
         }
-      x_space_test[j].index = -1;
-      x_space_test[j].value = 0;
-      env->vec_set(env, y_out, i, env->make_float(env, svm_predict(model, x_space_test)));
+      test_nodes[j].index = -1;
+      test_nodes[j].value = 0;
+      env->vec_set(env, y_out, i, env->make_float(env, svm_predict(model, test_nodes)));
     }
 
   return y_out;
@@ -146,7 +147,7 @@ emacs_module_init (struct emacs_runtime *ert)
   emacs_value vfun = env->make_function(env, 0, 0, Flibsvm_version, "Return the version of libsvm", NULL);
   bind_function(env, "esvm--libsvm-version", vfun);
 
-  emacs_value gfun = env->make_function(env, 3, 3, Fgo, "Go go go", NULL);
+  emacs_value gfun = env->make_function(env, 3, 3, Fgo, "Train and predict on the given data", NULL);
   bind_function(env, "esvm--go", gfun);
 
   provide(env, "esvm-core");
